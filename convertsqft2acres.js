5876f7960dcf13cea19c3aa95608b7271e703abd
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-02-28 01:28:17 +0000

    populate q1 and its spec

---
+++ convertsqft2acres.js
@@ -0,0 +1,6 @@
+function convertsqft2acres(sqft){
+    let acres = sqft/43560
+    return acres;
+ }
+ 
+ export {convertsqft2acres} 
---
+++ spec/convertsqft_spec.js
@@ -0,0 +1,21 @@
+import {convertsqft2acres} from "../convertsqft2acres.js";
+
+describe("test convertsqft2acres", function() {
+   it("tests 100 000 sqft", function() {
+       let sqft = 100000;
+       let acres = convertsqft2acres(sqft);
+   expect(acres.toFixed(2)).toBe("2.30");
+   });
+ 
+   it("tests 50 000 sqft", function() {
+       let sqft = 50000;
+       let acres = convertsqft2acres(sqft);
+   expect(acres.toFixed(2)).toBe("1.15");
+   });
+
+   it("tests 250 000 sqft", function() {
+       let sqft = 250000;
+       let acres = convertsqft2acres(sqft);
+   expect(acres.toFixed(2)).toBe("5.74");
+   });
+});
