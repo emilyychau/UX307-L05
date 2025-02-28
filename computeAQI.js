1df5d0976a5341d016ff9e82391d1ba7f2fa8b6f
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-02-28 02:29:44 +0000

    fix spec for q3

--- computeAQI.js
+++ computeAQI.js
@@ -7,16 +7,16 @@ function computeAQI(fAQI){
     else if (fAQI >= 0 && fAQI <= 50){
        sAQ = "The air quality is good";
     }
-    else if (fAQI >= 51 && 100){
+    else if (fAQI >= 51 && fAQI <= 100){
        sAQ = "The air quality is moderate";
     }
-    else if (fAQI >= 101 && 150){
+    else if (fAQI >= 101 && fAQI <= 150){
       sAQ = "The air quality is unhealthy for sensitive groups";
     }
-    else if (fAQI >= 151 && 200){
+    else if (fAQI >= 151 && fAQI <= 200){
       sAQ = "The air quality is unhealthy";
     }
-    else if (fAQI >= 201 && 300){
+    else if (fAQI >= 201 && fAQI <= 300){
       sAQ = "The air quality is very unhealthy";
     }
     else if (fAQI >= 301){
@@ -25,6 +25,7 @@ function computeAQI(fAQI){
     else {
       sAQ = "Computation error, please enter an air quality value that is equal or greater than 0";
     }
+    return sAQ;
  }    
 
  export {computeAQI}
--- spec/computeAQI_spec.js
+++ spec/computeAQI_spec.js
@@ -4,36 +4,36 @@ describe("test AQI computation", function() {
     it("tests for an AQI of 25", function() {
       let fAQI = 25; 
       let sAQ = computeAQI(fAQI);
-   expect(sAQ).toEqual("The air quality is good");
+   expect(sAQ).toBe("The air quality is good");
    });
 
    it("tests for an AQI of 65", function() {
       let fAQI = 65;
       let sAQ = computeAQI(fAQI);
-   expect(sAQ).toEqual("The air quality is moderate");
+   expect(sAQ).toBe("The air quality is moderate");
    });
 
    it("tests for an AQI of 125", function() {
       let fAQI = 125;
       let sAQ = computeAQI(fAQI);
-   expect(sAQ).toEqual("The air quality is unhealthy for sensitive groups");
+   expect(sAQ).toBe("The air quality is unhealthy for sensitive groups");
    });
 
    it("tests for an AQI of 165", function() {
       let fAQI = 165;
       let sAQ = computeAQI(fAQI)
-   expect(sAQ).toEqual("The air quality is unhealthy");
+   expect(sAQ).toBe("The air quality is unhealthy");
    });
 
    it("tests for an AQI of 250", function() {
       let fAQI = 250;
       let sAQ = computeAQI(fAQI);
-   expect(sAQ).toEqual("The air quality is very unhealthy");
+   expect(sAQ).toBe("The air quality is very unhealthy");
    });
 
    it("tests for an AQI of 305", function() {
       let fAQI = 305;
       let sAQ = computeAQI(fAQI);
-   expect(sAQ).toEqual("The air quality is hazardous");
+   expect(sAQ).toBe("The air quality is hazardous");
    });
 })
